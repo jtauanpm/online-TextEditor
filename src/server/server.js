3b2f62ce07e13d7c340d1f039c162798f2611f45
Jonathan Tauan Pereira Maia <jtauan10@gmail.com>
2022-05-22 10:18:47 -0300

    create express app
    Co-Authored-By: Diogo Víctor <104800101+DiogoNogueira2002@users.noreply.github.com>
    Co-Authored-By: Anderson Azevedo da Silva <104504983+andersonazvd@users.noreply.github.com>

---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const ws = require('socket.io')(server);
+
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+app.use('/', (req, res)=> {
+    res.render('./home');
+})
+
+//app.get('/:id')
+
+const port = 8080;
+app.listen(port, () => console.log(`online server port ${port}`));
